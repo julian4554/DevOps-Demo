fe251619f32eac25d8931040e6b3fba6dc626a64
Julian Bick <julian.bick@googlemail.com>
2025-10-22 15:57:42 +0200

    Working CI/CD Pipeline 1.0

--- src/main/java/com/webcore/solutions/WebCoreApplication.java
+++ src/main/java/com/webcore/solutions/WebCoreApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebCoreAprlication {
+public class WebCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebCoreApplication.class, args);
     }
