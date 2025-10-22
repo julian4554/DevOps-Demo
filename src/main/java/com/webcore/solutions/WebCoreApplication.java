3022323f616bc19bedfed518e0b09434adad3b24
Julian Bick <julian.bick@googlemail.com>
2025-10-22 12:29:23 +0200

    False DB PW on purpose to learn CI/CD Behaviour

--- src/main/java/com/webcore/solutions/WebCoreApplication.java
+++ src/main/java/com/webcore/solutions/WebCoreApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebCoreApplrication {
+public class WebCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebCoreApplication.class, args);
     }
