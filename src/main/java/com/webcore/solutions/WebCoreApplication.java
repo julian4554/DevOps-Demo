b0b81a4ac74f8af2cc9ad5ebe113293730347158
Julian Bick <julian.bick@googlemail.com>
2025-10-22 15:55:44 +0200

    Break on purpose TESTING REASONS

--- src/main/java/com/webcore/solutions/WebCoreApplication.java
+++ src/main/java/com/webcore/solutions/WebCoreApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebCoreApplication {
+public class WebCoreAprlication {
     public static void main(String[] args) {
         SpringApplication.run(WebCoreApplication.class, args);
     }
